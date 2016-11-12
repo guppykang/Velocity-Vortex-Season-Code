b166ecb10393493c7585e1273ffef6222a47595e
David <dflores0818@gmail.com>
2016-11-12 09:51:42 -0800

    First commit, creates a class for the teleop.

---
+++ Griffins/src/main/java/org/firstinspires/ftc/griffins/Teleop.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.griffins;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by David on 11/12/2016 for Josh.
+ * Go ahead and start implementing a basic drive base.
+ */
+
+public class Teleop extends OpMode {
+}
